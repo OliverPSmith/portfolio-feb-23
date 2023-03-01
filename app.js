e25cf2301c2b1e5e5aba66229eaef82632a12537
Oliver Smith <77962172+OliverPSmith@users.noreply.github.com>
2023-03-01 23:18:40 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// Menu Toggle
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
+
+const landing = document.getElementById('landing');
+const homeInner = document.querySelector('.home-inner-content')
+const aboutInner = document.querySelector('.about-inner-content');
+const contactInner = document.querySelector('.contact-inner-content');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+
+    landing.classList.toggle('active');
+    homeInner.classList.toggle('active');
+    aboutInner.classList.toggle('active');
+    contactInner.classList.toggle('active');
+
+
+    for (let i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener('click', () => {
+            menuToggle.classList.remove('active');
+            nav.classList.remove('active');
+
+            landing.classList.remove('active');
+            homeInner.classList.remove('active');
+            aboutInner.classList.remove('active');
+            contactInner.classList.remove('active');
+        })
+    }
+})
