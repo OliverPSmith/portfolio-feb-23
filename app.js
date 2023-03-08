4f11bf45a93e41899e706c026d39690d53645f89
Oliver Smith <77962172+OliverPSmith@users.noreply.github.com>
2023-03-08 17:08:56 +0000

    new update JS

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const navLinks = Array.from(document.querySelectorAll('.nav-link'));
 
 const landing = document.getElementById('landing');
 const homeInner = document.querySelector('.home-inner-content')
+const websitesInner = document.querySelector('.projects-inner-content');
+const programmingInner = document.querySelector('.programming-inner-content');
 const aboutInner = document.querySelector('.about-inner-content');
 const contactInner = document.querySelector('.contact-inner-content');
 
@@ -15,6 +17,8 @@ menuToggle.addEventListener('click', () => {
 
     landing.classList.toggle('active');
     homeInner.classList.toggle('active');
+    websitesInner.classList.toggle('active');
+    programmingInner.classList.toggle('active');
     aboutInner.classList.toggle('active');
     contactInner.classList.toggle('active');
 
@@ -26,6 +30,8 @@ menuToggle.addEventListener('click', () => {
 
             landing.classList.remove('active');
             homeInner.classList.remove('active');
+            websitesInner.classList.remove('active');
+            programmingInner.classList.remove('active');
             aboutInner.classList.remove('active');
             contactInner.classList.remove('active');
         })
